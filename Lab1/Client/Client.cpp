14017b212a8b58e5659e43aac8c344c14f70f885
Pavel Klimanski <74463319+pavkliman@users.noreply.github.com>
2022-11-10 19:16:18 +0300

    Add files via upload

---
+++ Lab1/Client/Client.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <cmath>
+#include <cstdlib>
+#include "ClientTCP.h"
+
+double function(double x) {
+	return  (exp(x * cos(x))) / (x * x + 1);
+}
+
+double methodTrapezoid(double x1, double x2, double y1, double y2) {
+	return (y1 + y2) * (x2 - x1) / 2;
+}
+
+int main()
+{
+	const int PORT = 5000;
+
+	std::string message;
+	double x1, x2, y1, y2;
+	double result = 0;
+
+	ClientTCP client(PORT);
+	message = client.receiveMessage(15);
+
+	x1 = atof(message.substr(0, message.find(' ')).c_str());
+	x2 = atof(message.substr(message.find(' ') + 1, message.length() - message.find(' ') - 1).c_str());
+	y1 = function(x1);
+	y2 = function(x2);
+
+	std::cout << "A:    " << x1 << std::endl;
+	std::cout << "B:    " << x2 << std::endl;
+	std::cout << "F(A): " << y1 << std::endl;
+	std::cout << "F(B): " << y2 << std::endl;
+
+	result = methodTrapezoid(x1, x2, y1, y2);
+
+	std::cout << "Square: " << result << std::endl;
+
+	client.sendMessage(std::to_string(result));
+
+	int tmp = getchar();
+
+	return 0;
+}
---
+++ Lab1/Server/Server.cpp
@@ -0,0 +1,42 @@
+﻿#include "ServerTCP.h"
+
+int main()
+{
+	const int PORT = 5000;
+	int clientCount = 0;
+	std::string input;
+	std::string a;
+	std::string b;
+
+	do
+	{
+		std::cout << "Enter clients count:" << std::endl;
+		std::cin >> input;
+	} while (input[0] < '0' || input[0] > '9');
+
+	clientCount = stoi(input);
+
+	Server server(PORT, clientCount);
+
+	do
+	{
+		std::cout << "Enter A:" << std::endl;
+		std::cin >> a;
+	} while (a[0] == '-');
+
+	do
+	{
+		std::cout << "Enter B:" << std::endl;
+		std::cin >> b;
+	} while (b[0] == '-');
+
+	std::thread* threads = server.listenClients(stod(a), stod(b));
+
+	while (server.clientFinished != clientCount);
+
+	std::cout << "Square: " << server.getSquare() << std::endl;
+
+	int tmp = getchar();
+
+	return 0;
+}
